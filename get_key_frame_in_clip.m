967de0315cfa1eba5dff3a21a5fcec6db03f490d
Krazzyzhall <sshukla5@asu.edu>
2015-11-25 01:05:45 -0700

    Update get_key_frame_in_clip.m
    changed comparision technique to mean suqared distance

--- get_key_frame_in_clip.m
+++ get_key_frame_in_clip.m
@@ -1,33 +1,17 @@
-function [histoclip,ind]=get_key_frame_in_clip(videoclip, thresh)
+function [histoclip]=get_key_frame_in_clip(videoclip)
 %videoseq is a structure that holds all the frames and their frame number
 %your task is to calculate  the histogram for those images and save it into
 %the matrix to as information also save another matrix depicting the change
 % in consequtive images if the change is more mark it as key image
 % threshhold will be between 0 and 1
 [n,~]=size(videoclip);
-    tempimg=videoclip{1,1};
-    [imgm,imgn]=size(tempimg);
-temphist(:,1)=imhist(tempimg(:,:,1));
-temphist(:,2)=imhist(tempimg(:,:,2));
-temphist(:,3)=imhist(tempimg(:,:,3));
-cliphist(1,:,:)=temphist;
-changehist(1)=0;
 for i=2:n
-    tempimg=videoclip{i,1};
-temphist(:,1)=imhist(tempimg(:,:,1));
-temphist(:,2)=imhist(tempimg(:,:,2));
-temphist(:,3)=imhist(tempimg(:,:,3));
-cliphist(i,:,:)=temphist;
-din=sum(sum(sum(tempimg~=0)));
-changehist(i)=sum(sum(((cliphist(i,2:256,:)/din)-(cliphist(i-1,2:256,:)/din)).^2));
+    mot_vect_2=videoclip{i,2};
+    mot_vect_1=videoclip{i-1,2};    
+    diff_mot_vect(i-1)=sum(sum(sqrt((mot_vect_2{2}-mot_vect_1{2}).^2 + (mot_vect_2{1}-mot_vect_1{1}).^2)));
 end
 %% look for change in consequtive frames
-tind=changehist>thresh;
-if(sum(tind)==0)
-    ind=[];
-    histoclip={cliphist,videoclip{:,2}};
-else
-    ind=videoclip{tind,2};
-    histoclip={cliphist,videoclip{:,2}};    
+[~,ind]=max(diff_mot_vect);
+    histoclip={videoclip{ind,1},videoclip{ind,2},videoclip{ind,3}};    
 end
 
