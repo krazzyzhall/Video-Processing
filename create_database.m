7363639d0d28e1a4c46c4c14250d678ac747113a
Krazzyzhall <sshukla5@asu.edu>
2015-11-25 01:24:14 -0700

    Update create_database.m

--- create_database.m
+++ create_database.m
@@ -2,12 +2,16 @@ function create_database(dirname)
 % creates database based on the files present under dirname
 cd(dirname);
 filenames=dir;
+cd('..')
 for i=3:size(filenames,1)
-   vectors=video_to_vector(filenames(i));
+    sprintf('starting video to convert to vector....');
+   fillocName=['.\Videos\',filenames(i).name];
+   vectors=video_to_vector(fillocName);
+    sprintf(' video to convert to vector complete....');
    matName=[filenames(i).name(1:end-4) '_vectors.mat'];
    save(matName,'vectors');
    source=['C:\Users\sshukla5\Documents\Video-Processing-master\Videos\' matName]
    copyfile(source,'T:\VideoToVectors');
+    sprintf('moved mat file');
 end
-cd('..');
 end
